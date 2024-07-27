71edca922c246adb0f38935d7a1785c1b0316991
visteen <visteen192@gmail.com>
2024-07-27 09:38:37 +0000

    background image added

--- gane/App.js
+++ gane/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.container}>
-      <StartGameScreen/>
+      <ImageBackground 
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.container}
+        imageStyle={styles.backgroudImage}>
+        <StartGameScreen/>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,4 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  backgroudImage:{
+    opacity:0.25
+  }
 });
